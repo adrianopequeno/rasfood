a2752c45ea4c238bde2dbf7bf8eb0be5c2afb02e
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-10-17 21:59:37 -0300

    testing the DAO methods

--- src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
+++ src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
@@ -17,12 +17,30 @@ public class PratoService {
         risoto.setDisponibilidade(true);
         risoto.setValor(BigDecimal.valueOf(88.50));
 
+        Prato salmao = new Prato();
+        salmao.setId(2);
+        salmao.setNome("Salmao ao molho de maracuja");
+        salmao.setDescricao("Salmao grelhado ao molho de maracuja");
+        salmao.setDisponibilidade(true);
+        salmao.setValor(BigDecimal.valueOf(60.00));
+
         EntityManager entityManager = JPAUtil.getEntityManagerRasFood();
         PratoDao pratoDao = new PratoDao(entityManager);
         entityManager.getTransaction().begin();
         pratoDao.cadastrar(risoto);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        entityManager.flush();
+        pratoDao.cadastrar(salmao);
+        entityManager.flush();
+        System.out.println("O Prato consultado foi: " + pratoDao.consultar(2));
+
+        pratoDao.excluir(risoto);
+        System.out.println("O Prato consultado foi: " + pratoDao.consultar(1));
+        //entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        salmao.setValor(BigDecimal.valueOf(50.00));
+        pratoDao.atualizar(salmao);
+        System.out.println("O Prato atualizado foi: " + pratoDao.consultar(2));
 
     }
 }
